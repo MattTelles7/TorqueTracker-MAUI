55787e455e234b0f2856fe9885aefa031a042b91
Kritika-333 <sanjelka@mail.uc.edu>
2026-08-05 14:20:55 -0700

    feat: add vehicle list sorting and count

--- tellescm_MD-Final/tellescm_MD-Final/ViewModels/VehiclesViewModel.cs
+++ tellescm_MD-Final/tellescm_MD-Final/ViewModels/VehiclesViewModel.cs
@@ -1,4 +1,6 @@
 using System.Collections.ObjectModel;
+using System.Linq;
+using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using tellescm_MD_Final.Models;
 using tellescm_MD_Final.Repositories;
@@ -9,6 +11,29 @@ namespace tellescm_MD_Final.ViewModels;
 public partial class VehiclesViewModel(IVehicleRepository vehicleRepository) : BaseViewModel
 {
     public ObservableCollection<Vehicle> Vehicles { get; } = [];
+
+    public List<string> SortOptions { get; } =
+    [
+        "Name (A-Z)",
+        "Mileage (High-Low)"
+    ];
+
+    private string? selectedSortOption;
+
+    public string? SelectedSortOption
+    {
+        get => selectedSortOption;
+        set
+        {
+            if (SetProperty(ref selectedSortOption, value))
+            {
+                SortVehicles();
+            }
+        }
+    }
+
+    public int VehicleCount => Vehicles.Count;
+
 
     [RelayCommand]
     public async Task LoadAsync()
@@ -22,13 +47,17 @@ public partial class VehiclesViewModel(IVehicleRepository vehicleRepository) : B
         {
             IsBusy = true;
             ErrorMessage = string.Empty;
+
             var vehicles = await vehicleRepository.GetActiveVehiclesAsync();
 
             Vehicles.Clear();
+
             foreach (var vehicle in vehicles)
             {
                 Vehicles.Add(vehicle);
             }
+
+            OnPropertyChanged(nameof(VehicleCount));
         }
         catch (Exception)
         {
@@ -39,6 +68,31 @@ public partial class VehiclesViewModel(IVehicleRepository vehicleRepository) : B
             IsBusy = false;
         }
     }
+
+
+    private void SortVehicles()
+    {
+        IEnumerable<Vehicle> sortedVehicles = Vehicles;
+
+        if (SelectedSortOption == "Name (A-Z)")
+        {
+            sortedVehicles = Vehicles
+                .OrderBy(v => $"{v.Make} {v.Model}");
+        }
+        else if (SelectedSortOption == "Mileage (High-Low)")
+        {
+            sortedVehicles = Vehicles
+                .OrderByDescending(v => v.CurrentMileage);
+        }
+
+        Vehicles.Clear();
+
+        foreach (var vehicle in sortedVehicles)
+        {
+            Vehicles.Add(vehicle);
+        }
+    }
+
 
     [RelayCommand]
     private Task AddVehicleAsync() => Shell.Current.GoToAsync(nameof(AddVehiclePage));
